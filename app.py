b3b0d8ebc97a4057cc3698fc38352d7c5cd11d88
Junior Bortolanza <gbortolanzajr@gmail.com>
2024-09-06 21:09:06 -0300

    Updating CRUD Delete

--- app.py
+++ app.py
@@ -43,5 +43,38 @@ def show_user(user_id):
     print(type(user_id))
     return "%s" % user_id
 
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if task == None:
+        return jsonify({"message": "Não foi possível econtrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso!"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        print(t.to_dict())
+        if t.id == id:
+            task = t
+            break
+    if not task:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+    
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
+
+
 if __name__ == "__main__": 
     app.run(debug=True)
